b409b6591b9dd15e321b19fa6f825f894b8d085a
wrongu <richard.d.lange.13@dartmouth.edu>
2013-04-03 21:05:40 -0400

    fixed video boxes (add one to index) and parfor in annotate_all

--- Vid/annotate_all.m
+++ Vid/annotate_all.m
@@ -5,16 +5,17 @@ classes = dir(fullfile(top_dir, '10class'));
 
 for c=1:length(classes)
     class = classes(c);
-    if class.isdir && class.name(1) ~= '.'
-        vids = dir(fullfile(top_dir, '10class', class.name, '*.avi'));
-        for v=1:length(vids)
-            fprintf('%s%s\n', class.name, vids(v).name);
-            vid = VideoReader(fullfile(top_dir, '10class', class.name, vids(v).name));
+    cname = class.name;
+    if class.isdir && cname(1) ~= '.'
+        vids = dir(fullfile(top_dir, '10class', cname, '*.avi'));
+        parfor v=1:length(vids)
+            fprintf('%s%s\n', cname, vids(v).name);
+            vid = VideoReader(fullfile(top_dir, '10class', cname, vids(v).name));
             a = load(fullfile(top_dir, 'annot', ...
-                sprintf('%s%.2d.mat', class.name, v)));
+                sprintf('%s%.2d.mat', cname, v)));
             mov = box_video(vid, a.annotations);
             writer = VideoWriter(fullfile(top_dir, 'combined', ...
-                sprintf('%s%.2d_annot.avi', class.name, v)));
+                sprintf('%s%.2d_annot.avi', cname, v)));
         end
     end
 end
