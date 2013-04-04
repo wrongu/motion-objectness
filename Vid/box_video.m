9ec494f5baeca54ee70a834f6f844476d81e0181
wrongu <richard.d.lange.13@dartmouth.edu>
2013-04-04 10:54:30 -0400

    box scaling taken into account

--- Vid/box_video.m
+++ Vid/box_video.m
@@ -1,11 +1,11 @@
 % This function draws the bounding box(es) on the given video
 
-function annot_video = box_video(video, annotations, color)
+function annot_video = box_video(video, annotations, a_width, a_height, color)
     vprops = get(video, {'Height', 'Width', 'NumberOfFrames'});
     h=vprops{1}; w=vprops{2}; frames=vprops{3};
     assert(frames == length(annotations));
     
-    if (nargin < 3)
+    if (nargin < 5)
         color = zeros(1,1,3);
         color(1) = 1;
     else
@@ -20,11 +20,20 @@ function annot_video = box_video(video, annotations, color)
         cdata = read(video, f);
         % draw box
         a = annotations{f};
-        cdata(a.ytl+1:a.ybr+1, a.xtl+1, :) = repmat(color, [a.ybr-a.ytl+1 1 1]);
-        cdata(a.ytl+1:a.ybr+1, a.xbr+1, :) = repmat(color, [a.ybr-a.ytl+1 1 1]);
-        cdata(a.ytl+1, a.xtl+1:a.xbr+1, :) = repmat(color, [a.xbr-a.xtl+1 1 1]);
-        cdata(a.ybr+1, a.xtl+1:a.xbr+1, :) = repmat(color, [a.xbr-a.xtl+1 1 1]);
+        cdata = draw_box(cdata, ...
+            scale_box(video, [a.xtl a.ytl a.xbr a.ybr], a_width, a_height), color);
         % save frame
         annot_video(f).cdata = cdata;
     end
+end
+
+function img = draw_box(img, box, color)
+    xtl = box(1);
+    ytl = box(2);
+    xbr = box(3);
+    ybr = box(4);
+    img(ytl:ybr, xtl, :) = repmat(color, [ybr-ytl+1, 1, 1]);
+    img(ytl:ybr, xbr, :) = repmat(color, [ybr-ytl+1, 1, 1]);
+    img(ytl, xtl:xbr, :) = repmat(color, [xbr-xtl+1, 1, 1]);
+    img(ybr, xtl:xbr, :) = repmat(color, [xbr-xtl+1, 1, 1]);
 end
---
+++ Vid/scale_box.m
@@ -0,0 +1,11 @@
+% return box coordinates that fit on the video V, taken from annotations
+% that have width a_width and height a_height (w and h from 'annotation')
+% box and a_box should be [xtl ytl xbr ybr]
+function box = scale_box(V, a_box, a_width, a_height)
+    vdata = get(V, {'Width', 'Height'});
+    w = vdata{1};
+    h = vdata{2};
+    box = a_box;
+    box([1 3]) = box([1 3]) * w / a_width;
+    box([2 4]) = box([2 4]) * h / a_height;
+end
