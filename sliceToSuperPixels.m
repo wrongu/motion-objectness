b26e40b93d8937969947c3048a93107f226222f3
wrongu <richard.d.lange.13@dartmouth.edu>
2013-04-09 15:47:22 -0400

    runMotionObjectness takes img as input

--- sliceToSuperPixels.m
+++ sliceToSuperPixels.m
@@ -3,6 +3,6 @@ function N = sliceToSuperPixels(S, w, h)
     % S(k).points is a 2xP matrix where P is the number of points tracked.
     % Each column is a point [row; col] in the image, i.e. [y; x]
     for sp=1:length(S)
-        N(round(S(sp).points(1,:)), round(S(sp).points(2,:))) = sp;
+        N(floor(S(sp).points(1,:)+1), floor(S(sp).points(2,:))+1) = sp;
     end
 end
